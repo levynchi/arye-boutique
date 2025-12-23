611757f8d64972f756bb6c6d596a08f90e5d1cc3
lior levy <levynchi@gmail.com>
2025-12-23 13:42:31 +0200

    Add auto superuser creation script for Railway

---
+++ store/management/commands/create_superuser_if_missing.py
@@ -0,0 +1,41 @@
+"""
+Management command to create a superuser from environment variables.
+This is useful for Railway deployment where we can't access the shell easily.
+"""
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = 'Create a superuser if one does not exist'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+        
+        if not username or not password:
+            self.stdout.write(
+                self.style.WARNING('DJANGO_SUPERUSER_USERNAME or DJANGO_SUPERUSER_PASSWORD not set. Skipping superuser creation.')
+            )
+            return
+        
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(
+                self.style.SUCCESS(f'Superuser "{username}" already exists. Skipping.')
+            )
+            return
+        
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+        
+        self.stdout.write(
+            self.style.SUCCESS(f'Superuser "{username}" created successfully!')
+        )
+
